79995cd4036519e32ee099eee8b427da3ed7f809
eggtart93 <ktalex1993@gmail.com>
2015-08-15 16:56:45 -0400

    Revert "Modify div.py file"
    This reverts commit 93ac187fe452e724d17c923a5e258fe31154122f.

--- div.py
+++ div.py
@@ -3,8 +3,8 @@
 def div(x,y):
     print "DummyCal: ", x, " / ", y 
     if y == 0 :
-        print "Error: denominator can not be 0!!!\n"
-        return "0"
+        print "DummyCal: ZeroDivisionError, y cannot be 0"
+        return None
     else:
         return x/y
 

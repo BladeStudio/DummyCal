cc8a06a0e494102efedb260a690dbe2fe304e522
eggtart93 <ktalex1993@gmail.com>
2015-08-15 14:53:14 -0400

    Added input module to main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 # This is the entry point of our software
 
+import sys
+from add import add
+from sub import sub
+from mul import mul
+from div import div
+
+def getUserInput():
+    x = input("Enter x: ")
+    y = input("Enter y: ")
+    op = input("""Select operator: 
+    1 for '+'
+    2 for '-'
+    3 for 'x'
+    4 for '/'
+    """)
+    return [x,y,op]
+
 if __name__ == "__main__":
     print "Hello Dummy Cal!"
     
-    # TO-DO: get input from user
-    #(x, y) = getInput()
-    
-    # TO-DO: Calculation
-    #result = cal(x, y)
+    # get input from user
+    result = None
+    while result is None:
+        (x, y, op) = getUserInput()
+        if op == 1: result = add(x, y)
+        elif op == 2: result = sub(x, y)
+        elif op == 3: result = mul(x, y)
+        elif op == 4: result = div(x, y)
+        else: print "Please enter a valid operator"
     
     # TO-DO
     #output(result)

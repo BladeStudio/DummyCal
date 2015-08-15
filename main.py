88e97bc9499a3b910f40ab9e542d818243df13b7
eggtart93 <ktalex1993@gmail.com>
2015-08-15 12:57:18 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is the entry point of our software
+
+if __name__ == "__main__":
+    print "Hello Dummy Cal!"
+    
+    # TO-DO: get input from user
+    #(x, y) = getInput()
+    
+    # TO-DO: Calculation
+    #result = cal(x, y)
+    
+    # TO-DO
+    #output(result)

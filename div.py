93ac187fe452e724d17c923a5e258fe31154122f
jhzheng90 <jhzheng90@gmail.com>
2015-08-15 16:13:21 -0400

    Modify div.py file

--- div.py
+++ div.py
@@ -3,8 +3,8 @@
 def div(x,y):
     print "DummyCal: ", x, " / ", y 
     if y == 0 :
-        print "DummyCal: ZeroDivisionError, y cannot be 0"
-        return None
+        print "Error: denominator can not be 0!!!\n"
+        return "0"
     else:
         return x/y
 
